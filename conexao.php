442fbd2a6bf6d5e42331a2d95ca687a3c9c02c8b
yasmin730 <yasmin.vitoria.silveira@escola.pr.gov.br>
2025-10-20 11:25:14 -0300

    Add files via upload

--- conexao.php
+++ conexao.php
@@ -1,16 +1,18 @@
 <?php
 
+// Configurações do banco
+$host    = "localhost";   // normalmente não precisa alterar
+$usuario = "root";        // substituir se seu usuário não for root
+$senha   = "";            // substituir se você tiver senha no MySQL
+$banco   = "yasmin_vitoria";       // substituir pelo nome do seu banco criado no phpMyAdmin
 
-$host    = "localhost"; 
-$usuario = "root";   
-$senha   = "";          
-$banco   = "yasmin_vitoria";     
+// Conexão MySQLi
+$conexao = mysqli_connect($host, $usuario, $senha, $banco);
 
-$conexao = mysqli_connect($localhost, $root, $senha, $banco);
 if (!$conexao) {
     die("Erro ao conectar: " . mysqli_connect_error());
 }
 
-
+// SENSITIVE CASE suportar acentos e Ç
 mysqli_set_charset($conexao, "utf8");
 ?>
